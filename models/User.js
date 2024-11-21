18f0966030d7b522286bb43b5546f9b52ae7eb69
Nikiita12 <137431828+Nikiita12@users.noreply.github.com>
2024-11-21 15:31:04 +0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('./sequelize'); // Assuming you have a separate file for Sequelize configuration
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+  }
+);
+
+module.exports = { User };
